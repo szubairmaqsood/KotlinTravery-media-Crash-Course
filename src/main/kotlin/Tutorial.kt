9e2f5267ddd904b6e0e38923345fe2d7bcd254ce
zubair-pc\zubair <szubair@gmail.com>
2021-06-09 15:31:56 +0500

    Println with string and number is done

---
+++ src/main/kotlin/Tutorial.kt
@@ -0,0 +1,18 @@
+/*
+Every programme start with main functions
+Structure of a function
+fun functionname(){
+   // Here we will write code
+}
+Use of println is shown there
+ */
+
+fun main(){
+    println("Hello World");
+    println("Hello Guys");
+    println();
+    println("What's up?");
+    println(26);
+
+
+}
