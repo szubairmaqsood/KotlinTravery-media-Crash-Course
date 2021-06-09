d37a51f36ed17feed54e59e7c639109fe1de0f8d
zubair-pc\zubair <szubair@gmail.com>
2021-06-09 17:24:57 +0500

    String is used with functions

---
+++ src/main/kotlin/Strings.kt
@@ -0,0 +1,9 @@
+/*
+ String is a data type where we can use function as well
+ */
+fun stringUsage(){
+    val myName:String= "Zubair";
+    println("My name is $myName");
+    println("My name in upperCase is ${myName.uppercase().reversed() }");
+
+}
--- src/main/kotlin/Tutorial.kt
+++ src/main/kotlin/Tutorial.kt
@@ -16,6 +16,5 @@ fun main(){
     valAndVarUsage();
     arthmaticOperations();
     decimalNumber();
-
-
+    stringUsage();
 }
